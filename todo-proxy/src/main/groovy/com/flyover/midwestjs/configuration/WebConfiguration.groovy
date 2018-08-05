e233654c53125ed30171501e377cd89cc081f7bc
Bruce Coddington <brucecoddington@gmail.com>
2018-08-04 23:41:54 -0500

    zuul proxy added and working as expected

--- todo-proxy/src/main/groovy/com/flyover/midwestjs/configuration/WebConfiguration.groovy
+++ todo-proxy/src/main/groovy/com/flyover/midwestjs/configuration/WebConfiguration.groovy
@@ -1,5 +1,6 @@
 package com.flyover.midwestjs.configuration
 
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.filter.CharacterEncodingFilter
@@ -7,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
+@EnableZuulProxy
 class WebConfiguration {
     static final String INDEX_HTML = 'index.html'
 
